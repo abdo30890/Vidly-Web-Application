323b8962328f310970e488ccd605a86573cf3773
adbdelrahman ahmed <abdo.30890@gmail.com>
2021-03-15 13:23:06 +0200

    Adding Profile Data

---
+++ Vidly.Wep.App/Migrations/202103151111094_AddingDrivingLicenseToApp.cs
@@ -0,0 +1,18 @@
+namespace Vidly.Wep.App.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddingDrivingLicenseToApp : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.AspNetUsers", "DrivingLicense", c => c.String(nullable: false, maxLength: 255));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.AspNetUsers", "DrivingLicense");
+        }
+    }
+}
